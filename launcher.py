efa07bf412fde0e8a0a5660284fdca599f90dfd3
xeroxYor <lbbas@126.com>
2020-03-26 16:30:58 +0800

    add loginfo

--- launcher.py
+++ launcher.py
@@ -2,6 +2,7 @@
 
 __author__ = 'xeroxYor'
 import sys
+import logging
 
 from PyQt5.QtWidgets import QApplication
 
@@ -12,6 +13,7 @@ from window.MainWindow import MainWindow
 
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='DebugInfo',level=logging.DEBUG)
     app = QApplication(sys.argv)
     mainWinow = MainWindow()
     mainWinow.show()
